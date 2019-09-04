4f399391946b44f947ac4ff3af45031471a945b6
Ivy <1615543941@11\qq.com>
2019-09-04 20:20:42 +0800

    Update 三数之和.py
    有点菜，这个简单的题弄了一天，自己没有专心，加油吧

--- 三数之和.py
+++ 三数之和.py
@@ -6,12 +6,26 @@ class Solution(object):
         """
         ans = []
         nums.sort()
-        elem = {}
-        for i in range(len(nums)):
-            elem[nums[i]] = i
-        for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                k = 0 - nums[i] - nums[j];
-                if elem.has_key(k) and elem[k] > j:
-                    ans.append([i,j,k])
+        i = 0
+        while i < (len(nums) - 2):
+            if nums[i] > 0:
+                break
+            j = i+1
+            k = len(nums)-1
+            while j < k :
+                if nums[i] + nums[j] + nums[k] > 0:
+                    k = k-1
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j = j+1
+                else:
+                    ans.append([nums[i],nums[j],nums[k]])
+                    while j<k and nums[j] == nums[j+1]:
+                        j = j+1
+                    while j<k and nums[k] == nums[k-1]:
+                        k = k-1    
+                    j = j+1
+                    k = k-1
+            while i < len(nums)-1 and nums[i] == nums[i+1]:
+                i = i+1
+            i=i+1
         return ans
