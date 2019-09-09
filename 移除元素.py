0d5a6299b2bf0a8de61022b6e0ba90b8f4b63334
Ivy <1615543941@11\qq.com>
2019-09-09 21:47:04 +0800

    Create 移除元素.py
    最好在一开始就想好各种边界条件，而不是边提交边修改

---
+++ 移除元素.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        count = 0
+        end = len(nums) - 1
+        i = 0
+        while i <= end:
+            if nums[i] == val:
+                while i < end and nums[end] == val:
+                    end = end - 1
+                    count = count + 1
+                nums[i] = nums[end]
+                count = count + 1
+                end = end - 1
+        return len(nums) - count
