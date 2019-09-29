1a8132cf64934012725aa59f3a911d669f0e5ad0
ivy <1615543941@qq.com>
2019-09-30 01:46:38 +0800

    蠢
    我是真的蠢，一个拷贝问题困扰了我一天

--- 解数独_new.py
+++ 解数独_new.py
@@ -1,3 +1,4 @@
+import copy
 class Cell:
     def __init__(self):
         self.value = 0
@@ -11,101 +12,111 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         print("----->")
-        cells = [[Cell() for i in range(9)] for j in range(9)]
+        self.cells = [[Cell() for i in range(9)] for j in range(9)]
         for i in range(0,9):
             for j in range(0,9):
-                if board[i][j] != '.' and not self.setcells(i,j,cells,int(board[i][j]) ) :
+                if board[i][j] != '.' and not self.setcells(i,j,int(board[i][j]) ) :
                     print("error",end='')
-                    print([i,j])
-                    for ix in range(0,9):
-                        for jy in range(0,9):
-                            print(cells[ix][jy].constrain)
-                    print(cells[i][j].constrain)
                     return
-                elif board[i][j] != '.':
-                        print([i,j],end='')
-                        print(cells[i][j].value)
-        
-        print("----->")
-        if not self.findEmptycell(cells):
+    
+       
+        if not self.findEmptycell():
+            print("error",end='')
             return
-        print("----->")
+        print("---->temp%s" %(self.cells[8][8].value))
+        
         for i in range(0,9):
             for j in range(0,9):
-                board[i][j] = str(cells[i][j].value)
+                board[i][j] = str(self.cells[i][j].value)
         
-        #print(board)
-
-
-
-    def setcells(self,x,y,cells,value):
-        if cells[x][y].value == value:
-            return True
-        if cells[x][y].possibilenum == 1:
-            return False
-        cells[x][y].value = value
-        cells[x][y].constrain = [1] *10
-        cells[x][y].constrain[value] = 0
-        cells[x][y].possiblenum = 1
         for i in range(0,9):
-            if (i != y and not self.toConstrain(x,i,cells,value)) or (i !=x and not self.toConstrain(i,y,cells,value)):
+            print(board[i])
+
+
+
+    def setcells(self,x,y,value):
+        if self.cells[x][y].value == value:
+            return True
+        if self.cells[x][y].constrain[value] == 1:
+            print("----->  1")
+            return False
+        self.cells[x][y].value = value
+        self.cells[x][y].constrain = [1] *10
+        self.cells[x][y].constrain[value] = 0
+        self.cells[x][y].possiblenum = 1
+        for i in range(0,9):
+            if (i != y and not self.toConstrain(x,i,value)) or (i !=x and not self.toConstrain(i,y,value)):
+                print("----->  2")
                 return False
+    
         for i in range(x//3*3,x//3*3+3):
             for j in range(y//3*3,y//3*3+3):
-                if (i != x and j!=y) and not self.toConstrain(i,j,cells,value):
-                    return False
-        return True
+                if (i != x or j != y) and not self.toConstrain(i,j,value):
+                    print("----->  3")
+                    return False    
+        return True    
 
-    def toConstrain(self,x,y,cells,value):  #传递constrain possible=1就尽早填入可以填的数减少回溯
-        if cells[x][y].constrain[value]:
+        
+
+    def toConstrain(self,x,y,value):  #传递constrain possible=1就尽早填入可以填的数减少回溯
+        if self.cells[x][y].constrain[value]:
             return True
-        if cells[x][y].value == value:
+        if self.cells[x][y].value == value:
+            print("----->  4")
             return False
-        cells[x][y].constrain[value] = 1
-        cells[x][y].possibilenum -= 1
-        if cells[x][y].possibilenum == 1:
-            for i in range(1,10):
-                if cells[x][y].constrain[i] == 0:
-                    return self.setcells(x,y,cells,i)
-        else:
+        self.cells[x][y].constrain[value] = 1
+        self.cells[x][y].possibilenum -= 1
+        if self.cells[x][y].possibilenum > 1:
             return True
+        if self.cells[x][y].possibilenum == 1:
+            for i in range(1,10):
+                if self.cells[x][y].constrain[i] == 0:
+                    return self.setcells(x,y,i)
+        return False
 
-    def mysort(self,cells,bt):      #按照能填入数字的多少从小到大排序
-        temp = []
+    def mysort(self,bt):      #按照能填入数字的多少从小到大排序
         for i in range(len(bt)):
             for j in range(len(bt)-1):
-                if cells[bt[j][0]][bt[j][1]].possibilenum > cells[bt[j+1][0]][bt[j+1][1]].possibilenum:
+                if self.cells[bt[j][0]][bt[j][1]].possibilenum > self.cells[bt[j+1][0]][bt[j+1][1]].possibilenum:
                     bt[j] ,bt[j+1] = bt[j+1] ,bt[j]
              
                     
-    def findEmptycell(self,cells):
+    def findEmptycell(self):
         bt = []
         for i in range(0,9):
             for j in range(0,9):
-                if cells[i][j].value == 0:
+                if self.cells[i][j].value == 0:
                     bt.append([i,j])
-        self.mysort(cells,bt)
-        return self.back_track(0,bt,cells)
+        self.mysort(bt)
+        print()
+        for i in range(len(bt)):
+            print("%d  " %(self.cells[bt[i][0]][bt[i][1]].possibilenum),end='')
+            print(bt[i])
+        return self.back_track(0,bt)
     
-    def back_track(self,k,bt,cells):
-        if k >= bt.size():
+    def back_track(self,k,bt):
+        if k >= len(bt):
             return True
         x = bt[k][0]
         y = bt[k][1]
-        if cells[x][y].value:
-            return self.back_track(k+1,bt,cells)
+        print("%d<--->%d"%(x,y))
+        if x == 2 and y == 0:
+            print("aaa")
+        if self.cells[x][y].value:
+            return self.back_track(k+1,bt)
         
-        temp = cells
+        tempconstrain = copy.deepcopy(self.cells[x][y].constrain)
+        temp = copy.deepcopy(self.cells)
         for v in range(1,10):
-            if not cells[x][y].constrain[v]:
-                if self.setcells(x,y,cells,v):
-                    if self.back_track(k+1,bt,cells):
+            if not tempconstrain[v]:
+                if self.setcells(x,y,v):
+                    if self.back_track(k+1,bt):
                         return True
-            cells = temp
+                self.cells = temp
         return False
 
 
 
 a = Solution()
-board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+board = [[".",".","9","7","4","8",".",".","."],["7",".",".",".",".",".",".",".","."],[".","2",".","1",".","9",".",".","."],[".",".","7",".",".",".","2","4","."],[".","6","4",".","1",".","5","9","."],[".","9","8",".",".",".","3",".","."],[".",".",".","8",".","3",".","2","."],[".",".",".",".",".",".",".",".","6"],[".",".",".","2","7","5","9",".","."]]
 a.solveSudoku(board)
