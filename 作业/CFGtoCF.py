0861acd702e8436dc6870c568738beeb7c89b8f5
ivy <1615543941@qq.com>
2019-11-23 22:08:10 +0800

    slove

--- 作业/CFGtoCF.py
+++ 作业/CFGtoCF.py
@@ -1,4 +1,4 @@
-1class nVar:
+class nVar:
     def __init__(self,Value =None,nextVar = None):
         self.Value = Value
         self.nextVar = []
@@ -15,6 +15,15 @@ class CFGtoCF:
         self.loadCFG()
         for i in self.Var:
             print(self.Var[i])
+        self.deleteE()
+        for i in self.Var:
+            print(self.Var[i])
+        self.removeUnit()
+        for i in self.Var:
+            print(self.Var[i])
+        self.introVar()
+        for i in self.Var:
+            print(i + "  :  " + str(self.Var[i]))
 
     def loadCFG(self):      #
         fo = open("CFG.txt",'r')
@@ -22,6 +31,7 @@ class CFGtoCF:
         i = 0
         j = 0
         flag = 0
+        self.Var["Z"] = []          #开始节点 先设置来占位
         while i < len(CFGstring):
             if CFGstring[i].isupper() and flag == 0:
                 key = CFGstring[i]
@@ -42,24 +52,103 @@ class CFGtoCF:
                     j = i
             else:
                 i += 1
-
+        self.Var["Z"].append(list(self.Var.keys())[1])
         fo.close()
 
     def deleteE(self):          #删除空epison
-        for key in self.Var:
-            for i in self.Var[key]:
-                if i == 'ξ':
-                    self.subdelete(key)
+        self.hasdel = []        #维护一个集合，里面装的是已经删除过的空产生事，防止重复产生空产生式
+        self.flag = 1                #flag = 1 表示有新的空产生式产生，需要重新遍历一次CFG
+        while(self.flag == 1):
+            self.flag = 0
+            for key in self.Var:
+                for i in self.Var[key]:
+                    if i == 'ξ':
+                        self.hasdel.append(key)
+                        self.Var[key].remove('ξ')       #先移除空产生式
+                        if len(self.Var[key]) == 0:
+                            del dict[key]
+                        self.subdelete(key)
 
     def subdelete(self,keyV):       #检测到有epison后从起始节点开始处理
         for key in self.Var:
             for i in self.Var[key]:
-                if keyV in i:               #用全排列来处理一个产生式里面有多个空产生式的情况
+                if keyV in i:            #用全排列来处理一个产生式里面有多个空产生式的情况
+                    mapdict = {}
+                    count = 0
+                    for j in range(len(i)):
+                        if i[j] == keyV:
+                            mapdict[count] = j
+                            count += 1
+                    self.combination = []
+                    self.combin(len(mapdict.keys()),0,[])
+                    self.combination.remove([])
+                    for tindexs in self.combination:
+                        tempexp = i
+                        for tindex in range(len(tindexs)-1,-1,-1):
+                            tempexp = tempexp[:mapdict[tindexs[tindex]]] + tempexp[mapdict[tindexs[tindex]]+1:]
+                        if len(tempexp) == 0 and key not in self.hasdel:
+                            self.Var[key].append('ξ')
+                            self.flag = 1
+                        elif tempexp not in self.Var[key]:
+                            self.Var[key].append(tempexp)
+
+    def combin(self,nums,sta,path):
+        self.combination.append(path[:])
+        for i in range(sta,nums):
+            path.append(i)
+            self.combin(nums,i+1,path)
+            path.pop()
+
+    def removeUnit(self):           #移除单元产生式
+        for keys in self.Var:           #先移除S->S
+            for exp in self.Var[keys]:
+                if exp == keys:
+                    self.Var[keys].remove(exp)
+
+        for keys in self.Var:
+            deltemp = []
+            addtemp = []
+            for exp in self.Var[keys]:
+                if len(exp) == 1 and exp.isupper():
+                    deltemp.append(exp)
+                    for i in self.Var[exp]:
+                        addtemp.append(i)
+            for i in deltemp:
+                self.Var[keys].remove(i)
+            for i in addtemp:
+                self.Var[keys].append(i)
+
+    def introVar(self):             #引入新的变量来解决ABCD...这种产生式
+        Termin = []
+        tempterdict = {}               #创建一个临时字典 方便通过终结符寻找key
+        tempvardict = {}
+        varcount = 0
+        tercount = 0
+
+        for key in list(self.Var.keys()):              #找出长度大于2的产生式 用新变量替代
+            for exp in range(len(self.Var[key])):
+                if len(self.Var[key][exp]) > 2:
+                    if self.Var[key][exp][1:] not in tempvardict.keys():
+                        newvar = "#V"+str(varcount)+"#"
+                        varcount += 1
+                        self.Var[newvar] = []
+                        self.Var[newvar].append(self.Var[key][exp][1:])
+                        tempvardict[self.Var[key][exp][1:]] = newvar
+                    self.Var[key][exp] = self.Var[key][exp][0] + tempvardict[self.Var[key][exp][1:]]
 
 
+        for key in list(self.Var):                                #替换终结符
+            for exp in range(len(self.Var[key])) :
+                for alpha in self.Var[key][exp]:
+                    if alpha.islower() and len(self.Var[key][exp]) > 1:
+                        if alpha not in tempterdict.keys():
+                            keys = "#T" + str(tercount) + "#"
+                            tercount += 1
+                            self.Var[keys] = []
+                            self.Var[keys].append(alpha)
+                            tempterdict[alpha] = keys
+                        self.Var[key][exp] = self.Var[key][exp].replace(alpha,tempterdict[alpha],1)
 
-    def combin(self,Var,keyV):
-        for i in range(len(self.))
 
 
 
